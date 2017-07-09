1d437764409b8a3d998f8e3ae4b5f32dceda70ae
Patrick Ziller <patrickandstuff@gmail.com>
2017-07-09 16:04:45 -0400

    set header height based on OS

--- config/router.js
+++ config/router.js
@@ -1,21 +1,37 @@
 import React from 'react'
+import { Platform} from 'react-native'
+import { Constants } from 'expo'
 import { StackNavigator } from 'react-navigation'
 
 import CommentsScreen from '../screens/CommentsScreen'
 import HomeScreen from '../screens/HomeScreen'
+
+const headerHeight = Constants.statusBarHeight + (Platform.OS === 'ios' ? 45 : 55)
 
 export const FeedStack = StackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
       title: 'HN',
-      headerBackTitle: 'Back'
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: 'orange',
+        paddingTop: Constants.statusBarHeight,
+        height: headerHeight
+      },
+      headerBackTitle: 'Back',
     }
   },
   Comments: {
     screen: CommentsScreen,
     navigationOptions: {
-      title: 'Comments'
+      title: 'Comments',
+      headerTintColor: 'white',
+      headerStyle: {
+        backgroundColor: 'orange',
+        paddingTop: Constants.statusBarHeight,
+        height: headerHeight
+      }
     }
   }
 });
